a8d1ee9adb321cf96e43b7b76df6010b1958e9ff
Abhijit <abhijitbcob96@gmail.com>
2021-06-05 12:52:15 +0530

    jsc

--- js/script.js
+++ js/script.js
@@ -4,15 +4,13 @@ if (window.document.body.clientWidth >= 800) {
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
-    mobileFirst: true,
   });
 } else {
   $(".slider").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000,
-    mobileFirst: true,
+    autoplaySpeed: 2000,
     dots: true,
   });
 }
