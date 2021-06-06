35b4b23ec41b6ae25df80918a5fd7b482be14239
Abhijit <abhijitbcob96@gmail.com>
2021-06-06 13:30:53 +0530

    made the carousel responsive

--- js/script.js
+++ js/script.js
@@ -1,21 +1,32 @@
 "use strict";
 
-if (window.document.body.clientWidth >= 800) {
-  $(".slider").slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-  });
-} else {
-  $(".slider").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    dots: true,
-  });
-}
+$(".slider").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
+});
 
 // Email validation
 const inputGroup = document.querySelector(".form-group");
