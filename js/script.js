7f20c5c59a12b10c786f84cd312055bdec5d2e2d
Abhijit <abhijitbcob96@gmail.com>
2021-06-06 07:03:17 +0530

    cc

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 if (window.document.body.clientWidth >= 800) {
   $(".slider").slick({
     slidesToShow: 3,
@@ -14,3 +16,31 @@ if (window.document.body.clientWidth >= 800) {
     dots: true,
   });
 }
+
+// Email validation
+const inputGroup = document.querySelector(".form-group");
+const emailInput = document.querySelector(".email-input");
+const submitBtn = document.querySelector(".submit-input");
+
+function ValidateEmail(inputText) {
+  const error = document.querySelector(".error");
+
+  var mailformat =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (inputText.value.match(mailformat)) {
+    error.style.display = "none";
+    emailInput.classList.remove("input-error");
+  } else {
+    error.style.display = "block";
+    emailInput.classList.add("input-error");
+  }
+}
+
+emailInput.addEventListener("keyup", function () {
+  ValidateEmail(emailInput);
+});
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  ValidateEmail(emailInput);
+});
